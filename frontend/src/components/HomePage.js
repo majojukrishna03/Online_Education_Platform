e810b5911d66296c1da193ab95a25ed75e80e518
majojukrishna03 <majojukrishna03@gmail.com>
2024-06-25 14:08:51 +0530

    added Homepage

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Online Education Platform</h1>
+      <p>Start your learning journey today!</p>
+      {/* Add additional content or features as needed */}
+    </div>
+  );
+};
+
+export default HomePage;
