38db8a5a554857b3d1f0f3002e38f64a6d3b2c6a
majojukrishna03 <majojukrishna03@gmail.com>
2024-06-25 19:43:51 +0530

    Updated Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import RegistrationPage from './components/RegistrationPage'; // Example import
+import LoginPage from './components/LoginPage'; // Example import
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HomePage />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/registration" element={<RegistrationPage/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            {/* Additional routes can be added here */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
